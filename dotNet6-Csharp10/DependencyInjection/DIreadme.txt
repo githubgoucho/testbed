e6fecd13bfa48600933fe49beb459314d30da577
geogoucho Live <geogoucho@hotmail.de>
2022-08-12 17:54:41 +0200

    Dependency Injection

---
+++ dotNet6-Csharp10/DependencyInjection/DIreadme.txt
@@ -0,0 +1,15 @@
+﻿/*
+ * Dependency Injection
+ * 
+ * Abhängigkeiten reduzieren, 
+ * Testbarkeit verbessern und 
+ * Wartungsfreuntlich 
+ * 
+ * 
+ * Umsetzung
+ * 
+ * Entkoppeln von Abhängigkeiten
+ * Contract Syncing
+ * IoC = Inversion of Control
+ * 
+ */
---
+++ dotNet6-Csharp10/DependencyInjection/Program.cs
@@ -0,0 +1,61 @@
+﻿// global usings in using.cs
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // main Fucking Point
+            var manager = new Manager(new Repository()); // <-- Dependency injection
+            Console.WriteLine(manager.Load());
+
+            Console.WriteLine(new Manager(new BlaBla()).Load()); // <-- Dependency injection
+            Console.ReadLine();
+        }
+    }
+
+    interface IRepository
+    {
+        string Load();
+    }    
+    
+    class Manager
+    {
+        private readonly IRepository _repository;
+
+        public Manager(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public string Load()
+        {
+            var input = _repository.Load();           
+            return input.ToUpper();
+        }
+    }
+
+
+    class BlaBla : IRepository
+    {
+        // without constructor
+        public string Load()
+        {
+            return "BlaBla";
+        }
+    }
+
+    class Repository:IRepository
+    {
+        public Repository()
+        {
+
+        }
+
+        public string Load()
+        {
+            return "berni";
+        }
+    }
+}
